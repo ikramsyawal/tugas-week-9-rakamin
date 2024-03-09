7c1a371c83a9d77d49caed17a165ab2d3a7252b9
Ikram Syawal Alitu <56379280+ikramsyawal@users.noreply.github.com>
2024-03-09 21:36:31 +0800

    create api documentation using swagger

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port = 3000;
 const router = require("./routes/router");
 const errorHandler = require("./middlewares/errorHandler");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(express.json());
 app.use("/api", router);
 app.use(errorHandler);
