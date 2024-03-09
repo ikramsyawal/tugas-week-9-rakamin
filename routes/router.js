b68066e4b5ab400c1f230f36b5af6cc099523a94
Ikram Syawal Alitu <56379280+ikramsyawal@users.noreply.github.com>
2024-03-09 12:43:54 +0800

    creating CRUD operations for user

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,17 @@
 const express = require("express");
+
+// Create a new router
 const router = express.Router();
 const moviesRouter = require("./moviesRouter.js");
+const userRouter = require("./userRouter.js");
 
+// Define the home page route
 router.get("/", (req, res) => {
   res.send("Welcome to the API");
 });
+
+// Define the movies and user routes
 router.use("/movies", moviesRouter);
+router.use("/users", userRouter);
 
 module.exports = router;
---
+++ routes/userRouter.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../db/queries");
+
+// get all users
+router.get("/", async (req, res, next) => {
+  try {
+    const allUsers = await pool.query("SELECT * FROM users");
+    res.status(200).json(allUsers.rows);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// get a user by id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    if (user.rows.length === 0) {
+      const error = new Error("User not found");
+      error.name = "ErrorNotFound";
+      throw error;
+    }
+    res.status(200).json(user.rows[0]);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// post a new user
+router.post("/", async (req, res, next) => {
+  try {
+    const { email, gender, password, role } = req.body;
+    const newUser = await pool.query(
+      "INSERT INTO users (email, gender, password, role) VALUES ($1, $2, $3, $4) RETURNING *",
+      [email, gender, password, role]
+    );
+    res.status(201).json({
+      message: "new user successfully created",
+      user: newUser.rows[0],
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// update a user
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { email, gender, password, role } = req.body;
+
+    const updatedUser = await pool.query(
+      "UPDATE users SET email = $1, gender = $2, password = $3, role = $4 WHERE id = $5 RETURNING *",
+      [email, gender, password, role, id]
+    );
+    if (updatedUser.rows.length === 0) {
+      const error = new Error("User not found");
+      error.name = "ErrorNotFound";
+      throw error;
+    }
+    res.status(200).json({
+      message: "User successfully updated",
+      user: updatedUser.rows[0],
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// delete a user
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await pool.query(
+      "DELETE FROM users WHERE id = $1 RETURNING *",
+      [id]
+    );
+    if (deletedUser.rows.length === 0) {
+      const error = new Error("User not found");
+      error.name = "ErrorNotFound";
+      throw error;
+    }
+    res.status(200).json({
+      message: "User successfully deleted",
+      user: deletedUser.rows[0],
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
